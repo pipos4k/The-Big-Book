a32518688d73ee243798b5900e465b63aaa48f51
pipos4k <kgeopap@gmail.com>
2025-03-12 20:10:42 +0200

    Improve the code?

--- 1.Bagel/bagel.py
+++ 1.Bagel/bagel.py
@@ -1,30 +1,45 @@
 import random
 
-# Creates 1 random numbers convert them to str, for each variable 
-x, y, z = str(random.randint(0, 9)), str(random.randint(0, 9)), str(random.randint(0, 9))
-code = [x, y, z]
+MAX_ATTEMPS = 10
+numbers_ = list("0123456789")
+random.shuffle(numbers_)
+code = ""
+for i in range(0, 3):
+    code += str(numbers_[i])
+
 print(code)
-
-guess = input("Enter 3 numbers: ")
-
+print('''Welcome to Bagel game. Im gonna think a number and you should find it.\nGood luck!
+Here are some tips for the game:
+Fermi means: Right place, right number.
+Pico means: Wrong place, right number.
+Bagel means: Nothing is correct.'''
+)
 
 def check(script, password):
     answers = []
+
+    if script == password:
+        return "You got it!!"
+
     for i in range(len(script)):
         if script[i] == password[i]:
             answers.append("Fermi")
-            break
         elif script[i] in password:
             answers.append("Pico")
-            continue
         if len(answers) == 0:
             answers = "Bagel"
-
     return(answers)
 
-answer = check(guess, code)
 
-print(f"{answer}")
+while MAX_ATTEMPS > 0:
+    guess = input("Enter 3 numbers: ")
 
+    answer = check(guess, code)
+    
+    MAX_ATTEMPS -= 1
+    print(f"{answer}")
+    print(f"You have {MAX_ATTEMPS} attemps.")
 
+if MAX_ATTEMPS == 0:
+    print("You lose.")
 
