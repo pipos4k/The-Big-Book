aab2cf7e6a87f41aaf8732d8ec796b13452e8d70
pipos4k <kgeopap@gmail.com>
2025-03-13 17:05:04 +0200

    Done with Bagel project

--- 1.Bagel/bagel.py
+++ 1.Bagel/bagel.py
@@ -1,13 +1,8 @@
 import random
+import re
 
-MAX_ATTEMPS = 10
-numbers_ = list("0123456789")
-random.shuffle(numbers_)
-code = ""
-for i in range(0, 3):
-    code += str(numbers_[i])
+pattern = r"\d{3}"
 
-print(code)
 print('''Welcome to Bagel game. Im gonna think a number and you should find it.\nGood luck!
 Here are some tips for the game:
 Fermi means: Right place, right number.
@@ -15,31 +10,64 @@ Pico means: Wrong place, right number.
 Bagel means: Nothing is correct.'''
 )
 
-def check(script, password):
+def generate_number():
+    # * Generator number, convert it to str
+    numbers_ = list("0123456789")
+    random.shuffle(numbers_)
+    code = ""   
+    for i in range(0, 3):
+        code += str(numbers_[i])
+
+    return code
+
+def check(user_guess, password):
+    # * Checks the code with user's guess and returns the answer
     answers = []
-
-    if script == password:
-        return "You got it!!"
-
-    for i in range(len(script)):
-        if script[i] == password[i]:
+    print(f"First answer: {answers}")
+    for i in range(len(user_guess)):
+        if user_guess[i] == password[i]:
             answers.append("Fermi")
-        elif script[i] in password:
+        elif user_guess[i] in password:
             answers.append("Pico")
+        else:
+            continue
+
         if len(answers) == 0:
-            answers = "Bagel"
+            return "Bagel"
     return(answers)
 
+def game():
+    MAX_ATTEMPS = 10
+    code = generate_number()
+    print(code)
 
-while MAX_ATTEMPS > 0:
-    guess = input("Enter 3 numbers: ")
+    while MAX_ATTEMPS > 0:
+        guess = input("Enter 3 numbers: ")
 
-    answer = check(guess, code)
+        if not re.fullmatch(pattern, guess):
+            print("Your input is not correct. Try again:")
+            continue
     
-    MAX_ATTEMPS -= 1
-    print(f"{answer}")
-    print(f"You have {MAX_ATTEMPS} attemps.")
+        if guess == code:
+            print("You got it!!")
+            break
 
-if MAX_ATTEMPS == 0:
-    print("You lose.")
+        answer = check(guess, code)
+        MAX_ATTEMPS -= 1
+        print(f"{answer}")
+        print(f"You have {MAX_ATTEMPS} attemps.")
+    
+    if MAX_ATTEMPS == 0:
+        print("You lose.")
 
+stop = True 
+
+while stop:
+    game()
+    answer = input("Keep playing? yes or no? \n")
+    if answer == "yes":
+        continue
+    else:
+        print("Good bye!")
+        stop = False
+        
