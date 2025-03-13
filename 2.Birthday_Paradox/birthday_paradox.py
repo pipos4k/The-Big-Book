c4b790b45168b29a33a9431acfdf8e027646b6e4
pipos4k <kgeopap@gmail.com>
2025-03-13 18:27:32 +0200

    Starts the Birthday Paradox problem

---
+++ 2.Birthday_Paradox/birthday_paradox.py
@@ -0,0 +1,28 @@
+import random
+from datetime import datetime
+import calendar
+
+def generate_day(quantity):
+
+    months = []
+
+    for i in range(quantity):
+        month_ = calendar.month_name[random.randint(1, 12)]
+        months_30 = ['4', '6', '9', '11']
+
+        if month_ in months_30:
+            day_ = str(random.randint(1, 30))
+        elif month_ == 2:
+            day_ = str(random.randint(1, 29))
+        else:
+            day_ = str(random.randint(1, 31))
+
+        birthday = [day_, month_]
+        months.append(birthday)
+
+    for i in months:
+        print(' '.join(i))
+
+
+generate_day(quantity=int(input("How many birthdays shall I generate? (Max 100)")))
+
