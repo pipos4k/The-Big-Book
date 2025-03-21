5c12dd7a6dda04064b9bfee27770365fe9b35f34
pipos4k <kgeopap@gmail.com>
2025-03-21 17:55:19 +0200

    Improves the Black Jack project

--- 4.Blackjack/blackjack.py
+++ 4.Blackjack/blackjack.py
@@ -6,23 +6,22 @@ DIAMONDS = chr(9830) # Character 9830 is '♦'.
 SPADES   = chr(9824) # Character 9824 is '♠'.
 CLUBS    = chr(9827) # Character 9827 is '♣'.
 # (A list of chr codes is at https://inventwithpython.com/charactermap)
-BACKSIDE = 'backside'
-
-# todo bets/double down/stand
-# todo win/lose/draw
+BACKSIDE = chr(35)
 
 # compares cards
-def compare_cards(user):
+def calculate_cards(user):
     calculate_cards = 0
+    if user[0][0] == "A" and user[1][0] == "A":
+        calculate_cards += 21
+        return calculate_cards
     for i in user:
         if i[0] == "A" or i[0] == "K" or i[0] == "Q" or i[0] == "J" or i[0] == "1":
             calculate_cards += 10
         else:
             calculate_cards += int(i[0])
-
     return calculate_cards
 
-# Makes the deck with 52 cards
+# Creates deck with 52 cards
 def make_deck():
     deck = []
     for i in range(2, 11):
@@ -34,11 +33,17 @@ def make_deck():
     random.shuffle(deck)
     return deck
 
-# Shares the cards for dealer and player
-def share_cards(hand):
+# Shares the cards for dealer and player as visual
+def share_visual_cards(hand):
     # Makes 2 lists, one to pop the cards and other to display them on the screen
     rows = ["", "", "", ""]
-   
+
+    if hand == dealers_hand:
+        for i in range(1):
+            rows[1] += (f"|{BACKSIDE}  |")
+            rows[2] += (f"| {BACKSIDE} |")
+            rows[3] += (f"|__{BACKSIDE}|")
+
     for line in range(len(hand)): # Makes the visual and put them on rows list
         rows[0] += (" ___ ")
         for i in range(1):
@@ -50,25 +55,74 @@ def share_cards(hand):
                 rows[1] += (f"|{hand[line][0]}  |")
                 rows[2] += (f"| {hand[line][2]} |")
                 rows[3] += (f"|__{hand[line][0]}|")
+        if hand == dealers_hand:
+            break
 
     for row in rows: # Prints them to monitor
         print(row)
 
-deck = make_deck()
+# Gives cards
+def give_cards(quantity, user_hand):
+    for i in range(quantity):
+        card = deck.pop()
+        user_hand.append(card)
+    
+# Calculate cards
+def calculate(player, dealer):
+    result = ""
+    global bet, money
 
-# Gives cards to player
-players_hand = []
-for i in range(2): 
-    card = deck.pop()
-    players_hand.append(card)
+    if player > 21:
+        result = (f"You lost {bet}!\n")
+        money -= bet
+    elif dealer == player:
+        result = "It's a tie\n"
+        # money += bet
+    elif dealer > player and dealer < 22:
+        result = (F"You lost {bet}!\n")
+        money -= bet
+    else:
+        result = (f"You win {bet*2}\n")
+        money += bet*2
 
-# Gives cards to dealer
-dealers_hand = []
-for i in range(2): 
-    card = deck.pop()
-    dealers_hand.append(card)
+    return result
 
-print(f"PLAYER'S HAND: {compare_cards(players_hand)}")
-share_cards(players_hand)
-print(f"DEALER'S HAND: {compare_cards(dealers_hand)}")
-share_cards(dealers_hand)
+money = 5000
+
+while money > 0:
+    deck = make_deck()
+
+    players_hand = []
+    dealers_hand = []
+    give_cards(2, players_hand)
+    give_cards(2, dealers_hand)
+
+    players_cards_sum = calculate_cards(players_hand)
+    dealers_cards_sum = calculate_cards(dealers_hand)
+    print(f"Money: {money}")
+    print("How much do you bet?")
+    bet = int(input())
+
+    print(f"DEALER: ??")
+    share_visual_cards(dealers_hand)
+    print(f"PLAYER: {players_cards_sum}")
+    share_visual_cards(players_hand)
+    print(dealers_cards_sum)
+
+
+    choice = input("(H)it, (S)tand, (D)ouble down: ")
+
+    if choice == "H":
+        if players_cards_sum > 21:
+            print("You overdraw")
+            continue
+        
+        give_cards(1, players_hand)
+        share_visual_cards(players_hand)
+
+    elif choice == "S":
+        print(calculate(players_cards_sum, dealers_cards_sum))
+
+   
+  
+      
